d11840c1c157ac76469af3fcf73ee8c0a294818e
Atil Samancioglu <samancioglu.atil@gmail.com>
2023-07-16 15:24:10 +0300

    Initial Commit

---
+++ NewStateManagement/NewStateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewStateManagementApp.swift
+//  NewStateManagement
+//
+//  Created by Atil Samancioglu on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewStateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
